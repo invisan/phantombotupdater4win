e69bbb18bc4cfbd697c6897a3ed94895d070cdbf
invisan <da_invisible@freenet.de>
2020-09-09 16:17:04 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -28,15 +28,15 @@ echo This is the Service Setting: %pservice%
 
 :main
 :: Get Newest Version Number
-if [%pbuild%]==[stable] curl -o stablebuild.xml --url "https://raw.githubusercontent.com/PhantomBot/PhantomBot/master/build.xml"
-if [%pbuild%]==[pbotde] curl -o pbotdebuild.xml --url "https://raw.githubusercontent.com/PhantomBotDE/PhantomBotDE/master/build.xml"
+if [%pbuild%]==[stable] curl -o build.xml --url "https://raw.githubusercontent.com/PhantomBot/PhantomBot/master/build.xml"
+if [%pbuild%]==[pbotde] curl -o build.xml --url "https://raw.githubusercontent.com/PhantomBotDE/PhantomBotDE/master/build.xml"
 if [%pbuild%]==[nightly] curl -o build.xml --url "https://raw.githubusercontent.com/PhantomBot/nightly-build/master/last_repo_version"
 goto versioncheck
 
 :versioncheck
 :: Get Version from installed PhantomBot 
 :: Copy PhantomBot.jar to get MetaInfo
-xcopy %ppath%\PhantomBot.jar
+xcopy %ppath%\PhantomBot.jar /C /Y /R
 :: Rename jar to zip to extract it with Powershell
 rename PhantomBot.jar PhantomBot.zip
 :: Extract content of Jar to get access to MetaInfo File
@@ -48,9 +48,30 @@ if NOT [%pbuild%]==[nightly] set "pvar=Version"
 for /F "tokens=2 delims=: " %%a in ('findstr /I "Implementation-%pvar%" extracted\META-INF\MANIFEST.MF') do set "pcurver=%%a"
 :: If the Build is nightly read the Implementations-Revision directly from build.xml
 if [%pbuild%]==[nightly] set /p pnewver=<build.xml
-if [%pbuild%]==[nightly] goto check2
+if [%pbuild%]==[nightly] goto checkver
 :: If the Build is non nightly use findstr to get the Version
-for /F "skip=0 tokens=2 delims==" %%a in ('findstr /I "value=" stablebuild.xml') do set "pnewver=%%a"
+for /F "tokens=5 delims== " %%a in ('findstr /I "name=.version..value=" build.xml') do set pnewver=%%a
+:: Remove quotes from the Variable.
+set pnewver=%pnewver:"=%
+:: Goto Version Check.
+goto checkver
 
-echo New Version is: %pnewver%
-echo Installed Version is: %pcurver%
+:checkver
+:: This is for the nightly Build
+:: Check if both Revisions/Versions are the same. If not download new Version. Else Skip.
+if [%pnewver%]==[%pcurver%] goto skip
+goto download
+
+
+:download
+:: Different Version on the Web. Downloading.
+if [%pbuild%]==[stable] curl -o bot.zip --url "https://github.com/PhantomBot/PhantomBot/releases/download/v%pnewver%/PhantomBot-%pnewver%-win.zip"
+if [%pbuild%]==[pbotde] curl -o bot.zip --url "https://github.com/PhantomBotDE/PhantomBotDE/releases/download/v%pnewver%/PhantomBotDE-%pnewver%.zip"
+if [%pbuild%]==[nightly] curl -o bot.zip --url "https://github.com/PhantomBot/nightly-build/raw/master/PhantomBot-nightly-lin.zip"
+
+:skip
+:: No New Version found. Restarting Bot.
+
+
+
+
